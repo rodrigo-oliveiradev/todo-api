c08771f6c879e967ba679bd293fcdb9bc83ca346
Rodrigo Oliveira <r.oliveira.95@hotmail.com>
2026-01-14 14:05:58 -0300

    feat: project structure setup and database connection configured

---
+++ src/main/java/com/oliveiradev/todo_api/domain/User.java
@@ -0,0 +1,30 @@
+package com.oliveiradev.todo_api.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false)
+    private String role;
+}
---
+++ src/main/java/com/oliveiradev/todo_api/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.oliveiradev.todo_api.repository;
+
+import com.oliveiradev.todo_api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
